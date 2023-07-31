0df3251e0aba46cce47fd82fdc389124f438599e
Manjiri Milind Chavande <manjiri.c@fyle.in>
2023-07-31 16:44:57 +0530

    fixed bug: Added parenthesis in the eof funciton call

--- frontend/parser.ts
+++ frontend/parser.ts
@@ -1,86 +1,109 @@
-import { tokenize, Token, TokenType } from "./lexer.ts";
-import { Stat, Program, Expr, BinaryExpr, NumericLiteral, Identifier } from "./ast.ts";
-
-export default class Parser {
-    private tokens: Token[] = [];
-
-    /*
-   * Determines if the parsing is complete and the END OF FILE Is reached.
+// deno-lint-ignore-file no-explicit-any
+import {
+    BinaryExpr,
+    Expr,
+    Identifier,
+    NumericLiteral,
+    Program,
+    Stat,
+  } from "./ast.ts";
+  
+  import { Token, tokenize, TokenType } from "./lexer.ts";
+  
+  /**
+   * Frontend for producing a valid AST from sourcode
    */
+  export default class Parser {
+    private tokens: Token[] = [];
+  
+    /*
+     * Determines if the parsing is complete and the END OF FILE Is reached.
+     */
     private not_eof(): boolean {
-        return this.tokens[0].type != TokenType.EOF;
+      return this.tokens[0].type != TokenType.EOF;
     }
-
+  
     /**
- * Returns the currently available token
- */
+     * Returns the currently available token
+     */
     private at() {
-        return this.tokens[0] as Token;
+      return this.tokens[0] as Token;
     }
-
+  
     /**
-  * Returns the previous token and then advances the tokens array to the next value.
-  */
+     * Returns the previous token and then advances the tokens array to the next value.
+     */
     private eat() {
-        const prev = this.tokens.shift() as Token;
-        return prev;
+      const prev = this.tokens.shift() as Token;
+      return prev;
     }
-
+  
     /**
-  * Returns the previous token and then advances the tokens array to the next value.
-  *  Also checks the type of expected token and throws if the values don't match.
-  */
+     * Returns the previous token and then advances the tokens array to the next value.
+     *  Also checks the type of expected token and throws if the values dnot match.
+     */
     private expect(type: TokenType, err: any) {
-        const prev = this.tokens.shift() as Token;
-        if (!prev || prev.type != type) {
-            console.error("Parser Error:\n", err, prev, " -Expecting: ", type);
-            Deno.exit(1);
-        }
-
-        return prev;
+      const prev = this.tokens.shift() as Token;
+      if (!prev || prev.type != type) {
+        console.error("Parser Error:\n", err, prev, " - Expecting: ", type);
+        Deno.exit(1);
+      }
+  
+      return prev;
     }
-
+  
     public produceAST(sourceCode: string): Program {
-        this.tokens = tokenize(sourceCode);
-        const program: Program = {
-            kind: "Program",
-            body: []
-        };
-
-        //parse until end of the file
-        while (this.not_eof) {
-            program.body.push(this.parseStat());
-        }
-        return program;
+      this.tokens = tokenize(sourceCode);
+      const program: Program = {
+        kind: "Program",
+        body: [],
+      };
+  
+      // Parse until end of file
+      while (this.not_eof()) {
+        program.body.push(this.parse_stmt());
+      }
+  
+      return program;
     }
-
-    //entry point for the parser
-    private parseStat(): Stat {
-        //skip to parse expr
-        return this.parse_expr();
+  
+    // Handle complex statement types
+    private parse_stmt(): Stat {
+      // skip to parse_expr
+      return this.parse_expr();
     }
-
+  
+    // Handle expressions
     private parse_expr(): Expr {
-        //parse binary expression
-        return this.parse_primary_expr();
-
+        return this.parse_primary_expr()
     }
-
+    
+    // Orders Of Prescidence
+    // AdditiveExpr
+    // MultiplicitaveExpr
+    // PrimaryExpr
+  
+    // Parse Literal Values & Grouping Expressions
     private parse_primary_expr(): Expr {
-        const tk = this.at().type;
-
-        switch (tk) {
-            case TokenType.Identifier:
-                return { kind: "Identifier", symbol: this.eat().value } as Identifier;
-
-            case TokenType.Number:
-                return {
-                    kind: "NumericLiteral",
-                    value: parseFloat(this.eat().value),
-                } as NumericLiteral;
-            default:
-                console.error("Unexpected error found during parsing", this.at());
-                Deno.exit(1);
-        }
+      const tk = this.at().type;
+  
+      // Determine which token we are currently at and return literal value
+      switch (tk) {
+        // User defined values.
+        case TokenType.Identifier:
+          return { kind: "Identifier", symbol: this.eat().value } as Identifier;
+  
+        // Constants and Numeric Constants
+        case TokenType.Number:
+          return {
+            kind: "NumericLiteral",
+            value: parseFloat(this.eat().value),
+          } as NumericLiteral;
+    
+        // Unidentified Tokens and Invalid Code Reached
+        default:
+          console.error("Unexpected token found during parsing!", this.at());
+          Deno.exit(1);
+      }
     }
-}
+  }
